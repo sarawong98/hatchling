1f28c5cef62573410c1af4cea5d533be6fb654fc
sarawong98 <swong@stud.hs-bremen.de>
2024-06-27 20:30:38 +0200

    move text more central

--- Gameover.js
+++ Gameover.js
@@ -4,11 +4,11 @@ export default class Gameover extends Phaser.Scene {
     }
 
     create(data) {
-        this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, 'GAMEOVER', {
+        this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2 - 64, 'GAMEOVER', {
             fontSize: '64px',
             fill: '#ff0000'
         }).setOrigin(0.5);
-        this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 1.7, 'Total: ' + data.finalScore, {
+        this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 1.7 - 64, 'Total: ' + data.finalScore, {
             fontSize: '48px',
             fill: '#ff0000'
         }).setOrigin(0.5);
