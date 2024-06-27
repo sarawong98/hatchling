0c9625a9ee9133e789741ea2b56addcdb9f3b699
sarawong98 <swong@stud.hs-bremen.de>
2024-06-27 20:03:09 +0200

    add own collide function in update

--- Flyinggame.js
+++ Flyinggame.js
@@ -7,10 +7,11 @@ export default class Flyinggame extends Phaser.Scene {
         this.scoreText = null;
         this.dragonFrames = [];
         this.frameIndex = 0;
-        this.hoopSpeed = 100;
+        this.ringSpeed = 100;
         this.gameover = false;
         this.player = null;
-        this.hoops = null;
+        this.rings = null;
+        this.previousRingBack = null;
     }
 
     preload() {
@@ -19,35 +20,37 @@ export default class Flyinggame extends Phaser.Scene {
         this.load.svg('drache_mitte', 'Komponenten/Drache_rechts_mitte.svg');
         this.load.svg('drache_unten', 'Komponenten/Drache_rechts_unten.svg');
         this.load.image('sky', 'Komponenten/sky.png');
-        this.load.image('star', 'Komponenten/star.png');
-        this.load.image('hoop_front', 'Komponenten/Reifen_vorne.svg');
-        this.load.image('hoop_back', 'Komponenten/Reifen_hinten.svg');
+        this.load.svg('ring_back', 'Komponenten/Reifen_hinten.svg');
+        this.load.svg('ring_front', 'Komponenten/Reifen_vorne.svg');
     }
 
     create() {
-        score = 0;
+        this.score = 0;
 
         var image = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'sky');
         var scaleX = this.cameras.main.width / image.width;
         var scaleY = this.cameras.main.height / image.height;
         var scale = Math.max(scaleX, scaleY);
-        image.setScale(scale).setScrollFactor(0).setDepth(0);
+        image.setScale(scale).setScrollFactor(0);
 
         this.dragonFrames = [
-            this.add.image(400, 500, 'drache_unten').setVisible(false).setScale(0.75).setDepth(2),
-            this.add.image(400, 500, 'drache_mitte').setVisible(false).setScale(0.75).setDepth(2),
-            this.add.image(400, 500, 'drache_mitte2').setVisible(false).setScale(0.75).setDepth(2),
-            this.add.image(400, 500, 'drache_oben').setVisible(false).setScale(0.75).setDepth(2),
-            this.add.image(400, 500, 'drache_mitte2').setVisible(false).setScale(0.75).setDepth(2),
-            this.add.image(400, 500, 'drache_mitte').setVisible(false).setScale(0.75).setDepth(2)
+            this.add.image(400, 500, 'drache_unten').setVisible(false).setScale(0.75).setDepth(1),
+            this.add.image(400, 500, 'drache_mitte').setVisible(false).setScale(0.75).setDepth(1),
+            this.add.image(400, 500, 'drache_mitte2').setVisible(false).setScale(0.75).setDepth(1),
+            this.add.image(400, 500, 'drache_oben').setVisible(false).setScale(0.75).setDepth(1),
+            this.add.image(400, 500, 'drache_mitte2').setVisible(false).setScale(0.75).setDepth(1),
+            this.add.image(400, 500, 'drache_mitte').setVisible(false).setScale(0.75).setDepth(1)
         ];
         this.dragon = this.dragonFrames[0];
         this.dragon.setVisible(true);
-        this.player = this.physics.add.group(this.dragon);
+        this.player = this.physics.add.sprite(this.dragon);
+
+        console.log('Drache: ' + JSON.stringify(this.dragon));
+        console.log('Spieler: ' + this.player);
 
         // Kollisionsbox des Drachen verkleinern
-        this.dragon.body.setSize(this.dragon.width * 0.5, this.dragon.height * 0.5); // Kollisionsbox auf 50% der Originalgröße setzen
-        this.dragon.body.setOffset(this.dragon.width * 0.25, this.dragon.height * 0.25); // Offset anpassen, falls notwendig
+        this.player.body.setSize(this.dragon.width * 0.5, this.dragon.height * 0.5); // Kollisionsbox auf 50% der Originalgröße setzen
+        this.player.body.setOffset(this.dragon.width * 0.25, this.dragon.height * 0.25); // Offset anpassen, falls notwendig
 
         // Input Events
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -63,23 +66,15 @@ export default class Flyinggame extends Phaser.Scene {
             loop: true
         });
 
-        // Generate hoops
-        this.hoops = this.physics.add.group();
-
-        this.spawnHoop();
+        // Generieren der Sterne
+        this.rings = this.physics.add.group();
 
         this.time.addEvent({
-            delay: 2000, // Generationszeit in ms
-            callback: this.spawnHoop,
+            delay: 3000, // Generationszeit in ms
+            callback: this.spawnRing,
             callbackScope: this,
             loop: true
         });
-
-        // Checks to see if the player overlaps with any of the hoops, if he does call the passThroughHoop function
-        this.physics.add.overlap(this.player, this.hoops.children, this.passThroughHoop, null, this);
-
-        console.log(this);
-
     }
 
     update() {
@@ -100,11 +95,29 @@ export default class Flyinggame extends Phaser.Scene {
             this.dragonFrames.forEach(frame => frame.y = this.dragon.y);
         }
 
-        this.hoops.children.iterate(function (hoop) {
-            if (hoop.x < 5) {
+        this.rings.children.iterate(function (ring) {
+            if (ring.texture.key === "ring_back") {
+                this.previousRingBack = ring;
+            }
+            if (ring.texture.key === "ring_front" && this.checkOverlap(ring)) {
+                console.log('overlapping');
+                ring.disableBody(true, true);
+                this.previousRingBack.disableBody(true, true);
+
+                // Score aktualisieren
+                this.score += 1;
+                this.scoreText.setText('Score: ' + this.score);
+            }
+            if (ring.x < 5) {
                 this.gameover = true;
             }
-        });
+        }, this);
+    }
+
+    checkOverlap(ring) {
+        var ringPassTolerance = 30;
+        // Annahme, dass x und y immer zentral liegen
+        return this.dragon.x >= (ring.x - 1) && this.dragon.x <= (ring.x + 1) && this.dragon.y >= (ring.y - ringPassTolerance) && this.dragon.y <= (ring.y + ringPassTolerance);
     }
 
     animateWings() {
@@ -114,27 +127,11 @@ export default class Flyinggame extends Phaser.Scene {
         this.dragon.setVisible(true);
     }
 
-    spawnHoop() {
-        const hoopBack = this.hoops.create(1880, Phaser.Math.Between(0, 800), 'hoop_back').setDepth(1);
-        hoopBack.setVelocityX(-this.hoopSpeed);
-        const hoopFront = this.hoops.create(1900, hoopBack.y, 'hoop_front').setDepth(3);
-        hoopFront.setVelocityX(-this.hoopSpeed);
-
-        const hoop = this.physics.add.group();
-        hoop.rear = hoopBack;
-        hoop.front = hoopFront;
-
-        this.hoops.add(hoop);
-    }
-
-    passThroughHoop(player, hoop) {
-        console.log(hoop)
-
-        // Disable the hoop parts
-        hoop.disableBody(true, true);
-
-        // Add and update the score
-        score += 1;
-        scoreText.setText('Score: ' + score);
+    spawnRing() {
+        const y = Phaser.Math.Between(50, this.cameras.main.height - 50);
+        const ringBack = this.rings.create(this.cameras.main.width - 40, y, 'ring_back').setDepth(0);
+        ringBack.setVelocityX(-this.ringSpeed);
+        const ringFront = this.rings.create(this.cameras.main.width - 20, y, 'ring_front').setDepth(1);
+        ringFront.setVelocityX(-this.ringSpeed);
     }
 }
