7ba00ce10d4fd665b6db80176d7820d4e5117441
sarawong98 <swong@stud.hs-bremen.de>
2024-06-30 17:43:22 +0200

    change sky background

--- Gameover.js
+++ Gameover.js
@@ -6,15 +6,19 @@ export default class Gameover extends Phaser.Scene {
     create(data) {
         this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2 - 64, 'GAMEOVER', {
             fontSize: '64px',
-            fill: '#ff0000'
+            fontWeight: 'bold',
+            fill: '#ffffff'
         }).setOrigin(0.5);
+
         this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 1.7 - 64, 'Total: ' + data.finalScore, {
             fontSize: '48px',
-            fill: '#ff0000'
+            fontWeight: 'bold',
+            fill: '#ffffff'
         }).setOrigin(0.5);
-        // Wechseln der Szene durch Leertaste
-        this.input.keyboard.on('keydown-SPACE', function () {
+
+        // Delay before restarting the main scene
+        this.time.delayedCall(3000, () => {
             this.scene.start('MainScene');
-        }, this);
+        });
     }
 }
